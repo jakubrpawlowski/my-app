8b8cfad2c90f0a024e608a5841d5f7e9dd3f5ca1
jakubrpawlowski <jakub.r.pawlowski@gmail.com>
2021-02-08 12:36:51 -0700

    Adds apple.

---
+++ pages/apple.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Apple() {
+  return (
+    <div>
+      <Head>
+        <title>Apple</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div>
+          <h1>Apple</h1>
+        </div>
+      </main>
+    </div>
+  );
+}
--- pages/bookstore.tsx
+++ pages/bookstore.tsx
@@ -2,7 +2,7 @@ import { Layer } from "components/Layer";
 import Head from "next/head";
 import styles from "styles/Home.module.css";
 
-export default function Home() {
+export default function Bookstore() {
   return (
     <div className={styles.container}>
       <Head>
@@ -16,17 +16,6 @@ export default function Home() {
           <Layer />
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
