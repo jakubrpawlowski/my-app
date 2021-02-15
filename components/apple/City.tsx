c5bf7ba98445bbd014e5b471b87ff4b468d92e5a
jakubrpawlowski <jakub.r.pawlowski@gmail.com>
2021-02-14 20:42:12 -0700

    Adds gkjohnson's shaders and geometries.

--- components/apple/City.tsx
+++ components/apple/City.tsx
@@ -1,7 +1,15 @@
 import { memo, Suspense } from "react";
 import { useLoader } from "react-three-fiber";
 import { greyscalePalette } from "resources/colors";
-import { MeshBasicMaterial } from "three";
+import { ConditionalEdgesGeometry } from "resources/ConditionalEdgesGeometry";
+import { ConditionalEdgesShader } from "resources/ConditionalEdgesShader";
+import {
+  EdgesGeometry,
+  LineBasicMaterial,
+  MeshBasicMaterial,
+  ShaderMaterial,
+} from "three";
+import { BufferGeometryUtils } from "three/examples/jsm/utils/BufferGeometryUtils.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 interface CityProps {}
@@ -11,10 +19,41 @@ const CityMesh = memo((props: CityProps) => {
   const building = nodes.building as THREE.Mesh;
 
   const material = new MeshBasicMaterial({
-    color: greyscalePalette.c[3],
+    polygonOffset: true,
+    polygonOffsetFactor: 1,
+    polygonOffsetUnits: 1,
+    colorWrite: false,
   });
 
-  return <mesh geometry={building.geometry} material={material}></mesh>;
+  // Threshold edges
+  const edgesGeometry = new EdgesGeometry(building.geometry, 40);
+  const lineMaterial = new LineBasicMaterial({
+    color: greyscalePalette.d[3],
+  });
+
+  // Conditional edges
+  const conditionalBuildingGeometry = building.geometry.clone();
+  for (const key in conditionalBuildingGeometry.attributes) {
+    if (key !== "position") {
+      conditionalBuildingGeometry.deleteAttribute(key);
+    }
+  }
+  const conditionalEdgesGeometry = new ConditionalEdgesGeometry(
+    BufferGeometryUtils.mergeVertices(conditionalBuildingGeometry)
+  );
+  const conditionalMaterial = new ShaderMaterial(ConditionalEdgesShader);
+  conditionalMaterial.uniforms.diffuse.value.set(greyscalePalette.d[3]);
+
+  return (
+    <>
+      <mesh geometry={building.geometry} material={material} />
+      <lineSegments geometry={edgesGeometry} material={lineMaterial} />
+      <lineSegments
+        geometry={conditionalEdgesGeometry}
+        material={conditionalMaterial}
+      />
+    </>
+  );
 });
 
 export const City = memo((props: CityProps) => {
