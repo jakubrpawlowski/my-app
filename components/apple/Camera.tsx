b63120d7f500bec2074352b8adad03519f210406
jakubrpalowski <jakub.r.pawlowski@gmail.com>
2021-02-18 18:27:25 -0700

    Adds custom camera.

---
+++ components/apple/Camera.tsx
@@ -0,0 +1,16 @@
+import { memo, useEffect, useRef } from "react";
+import { useThree } from "react-three-fiber";
+import { PerspectiveCamera } from "three";
+
+export const Camera = memo(() => {
+  const cameraRef = useRef<PerspectiveCamera>(null);
+  const { setDefaultCamera } = useThree();
+
+  // Make the camera known to the system
+  useEffect(() => {
+    if (cameraRef.current) {
+      setDefaultCamera(cameraRef.current);
+    }
+  }, []);
+  return <perspectiveCamera ref={cameraRef} position={[0, 0, 8]} />;
+});
--- components/apple/Dynamic.tsx
+++ components/apple/Dynamic.tsx
@@ -1,11 +1,13 @@
 import { City } from "components/apple/City";
 import { memo } from "react";
 import { Canvas } from "react-three-fiber";
+import { Camera } from "./Camera";
 import { Effects } from "./Effects";
 
 export const Dynamic = memo(() => (
   <Canvas>
     <City />
+    <Camera />
     <Effects />
   </Canvas>
 ));
